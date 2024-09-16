df213fa48cc3e68d14d0956b3ba73798565991db
Marcosluengo04 <marcos.lgortiz@gmail.com>
2024-09-16 20:20:14 +0200

    adasdhaiof
    dasdodh

--- Assets/Ejercicios/Ejercicio6_1.cs
+++ Assets/Ejercicios/Ejercicio6_1.cs
@@ -2,14 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Ejercicio1_1 : MonoBehaviour
+public class Ejercicio6_1 : MonoBehaviour
 {
-   
-
     // Start is called before the first frame update
     void Start()
     {
-       
+        
     }
 
     // Update is called once per frame
