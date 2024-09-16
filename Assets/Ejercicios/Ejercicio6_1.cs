82e9d2fa01fa7013da48843d4f15ebd59e3f4f4e
Marcosluengo04 <marcos.lgortiz@gmail.com>
2024-09-16 21:15:36 +0200

    adad
    dad

--- Assets/Ejercicios/Ejercicio6_1.cs
+++ Assets/Ejercicios/Ejercicio6_1.cs
@@ -4,10 +4,17 @@ using UnityEngine;
 
 public class Ejercicio6_1 : MonoBehaviour
 {
+    //6. EJERCICIOS DE OPERADORES LÓGICOS 
+    //6.1.Si X, Y y Z son condiciones con valores X = true, Y = false, Z = true, determina el  resultado de las siguientes expresiones lógicas:
+
+    [SerializeField] bool valorx;
+    [SerializeField] bool valory;
+    [SerializeField] bool valorz;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log()
     }
 
     // Update is called once per frame
