c2a84dd067f97b27086d17b8c74bdbaaebd3b56b
Marcosluengo04 <marcos.lgortiz@gmail.com>
2024-09-16 19:56:12 +0200

    dashdkla
    dakodhkoa

--- Assets/Ejercicios/Ejercicio1_1.cs
+++ Assets/Ejercicios/Ejercicio1_1.cs
@@ -4,10 +4,14 @@ using UnityEngine;
 
 public class Ejercicio1_1 : MonoBehaviour
 {
+    [SerializeField] bool valor1;
+    [SerializeField] bool valor2;
+    [SerializeField] bool valor3;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+       
     }
 
     // Update is called once per frame
