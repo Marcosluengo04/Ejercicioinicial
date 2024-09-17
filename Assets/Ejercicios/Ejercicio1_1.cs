61e7249b35b95ffbf555d9d12519f46c8d755c81
Marcosluengo04 <marcos.lgortiz@gmail.com>
2024-09-17 11:53:37 +0200

    Marcos
    adasd

--- Assets/Ejercicios/Ejercicio1_1.cs
+++ Assets/Ejercicios/Ejercicio1_1.cs
@@ -1,6 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
 using UnityEngine;
+//1. EJERCICIOS DE MENSAJES POR CONSOLA 
+//1.1.Realiza un código de tal manera que aparezca por consola el mensaje “¡Hola Mundo!”  al iniciar el juego. 
+//1.2.Realiza un código de tal manera que aparezca el mensaje anterior seguido del siguiente  mensaje: “Este es el primer videojuego de <tu_nombre>” al iniciar el juego. 
+//1.3.Por último, termina con una última frase del estilo: “Estoy aprendiendo C#”. 
+//1.4.Incluye con un Debug.Log el mensaje “Ha pasado un frame” dentro del bloque Update. 
+//1.5.Responde en cualquier parte del código con un comentario de bloque por qué aparece  tantas veces por consola este último mensaje al dar al Play. 
+
 
 public class Ejercicio1_1 : MonoBehaviour
 {
@@ -9,7 +18,9 @@ public class Ejercicio1_1 : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-       
+        Debug.Log("Hola Mundo");
+        Print("Este es el primer videojuego de Marcos Luengo Oritz" );
+        Debug.log("Estoy aprendiendo C#");
     }
 
     // Update is called once per frame
--- Assets/Ejercicios/Ejercicio3.cs
+++ Assets/Ejercicios/Ejercicio3.cs
@@ -2,15 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Ejercicio6_1 : MonoBehaviour
+public class Ejercicio3 : MonoBehaviour
 {
-    //6. EJERCICIOS DE OPERADORES LÓGICOS 
-    //6.1.Si X, Y y Z son condiciones con valores X = true, Y = false, Z = true, determina el  resultado de las siguientes expresiones lógicas:
-
     // Start is called before the first frame update
     void Start()
     {
-       
+        
     }
 
     // Update is called once per frame
