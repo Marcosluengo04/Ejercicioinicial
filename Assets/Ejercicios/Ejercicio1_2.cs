2517a128d3462ccf4b50404b207ab807c9ac89d5
Marcosluengo04 <marcos.lgortiz@gmail.com>
2024-09-17 12:33:46 +0200

    asdad
    aqsas

--- Assets/Ejercicios/Ejercicio1_2.cs
+++ Assets/Ejercicios/Ejercicio1_2.cs
@@ -1,13 +1,42 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
 using UnityEngine;
 
 public class Ejercicio1_2 : MonoBehaviour
 {
     // Start is called before the first frame update
+    //int edad;
+    //float altura;
+    //string nombre;
+    //float iva;
+    //int peso;
+    //bool alumnorepetidor;
+    //string letra;
+    //int minutos;
+    //string matriculadecoche;
+    //bool mayordeedad;
+    //int codigopostas;
+    //string genero;
+    //int numerodehijos;
+    //string talladeropa;
+    //int precio;
+    //string mensaje;
+    //int mayoredad;
+    //int dias;
+    //int contador;
+    //string talladecamisetas;
     void Start()
     {
-        
+         int vidas = 5;
+         float exp = 2.5;
+         char carac Hola;
+         Debug.Log("Resultado suma" vidas + exp);
+
+         Debug.Log( "Resultado resta" exp - vidas);
+
+
     }
 
     // Update is called once per frame
