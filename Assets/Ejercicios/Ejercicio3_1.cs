48f776ecaf145f05b05a0ff261d7230c24d00e73
Marcosluengo04 <marcos.lgortiz@gmail.com>
2024-09-18 18:15:45 +0200

    eq
    dad

--- Assets/Ejercicios/Ejercicio3_1.cs
+++ Assets/Ejercicios/Ejercicio3_1.cs
@@ -5,9 +5,16 @@ using UnityEngine;
 public class Ejercicio3_1 : MonoBehaviour
 {
     // Start is called before the first frame update
+    
     void Start()
     {
-        
+        int numero1 = 1;
+        int numero = 2;
+
+        Debug.Log(numero1);
+        Debug.Log(numero);
+
+
     }
 
     // Update is called once per frame
